dac9a9b1672c3861b681dfd969b67e9fb0e3dd9b
Artur <artur.zubkov111@gmail.com>
2023-10-04 19:53:13 +0300

    Полный рефакторинг кода

--- src/main/java/org/example/BigBrotherBot.java
+++ src/main/java/org/example/BigBrotherBot.java
@@ -1,284 +1,105 @@
 package org.example;
 
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class BigBrotherBot extends TelegramLongPollingBot {
 
-    int messageId = 0;
-
-    List<String> selectedWorkers = new ArrayList<>();
-    private Map<String, Boolean> buttonStates = new HashMap<>();
+    String building = "";
 
     public String getBotUsername() {
-        return "MY_BOTNAME";
+        return "ControlOfAttendance_Bot";
     }
 
     public String getBotToken() {
-        return "MY_TOKEN";
+        return "6521254218:AAHENeMjcXr7R6jphZ2EoU0Oa6drKRK58u8";
     }
 
+    @Override
     public void onUpdateReceived(Update update) {
 
-        if (update.hasMessage() && update.getMessage().getText().startsWith("/start")) {
-            try {
-                execute(SendMessage.builder().text("Бригада:")
-                        .chatId(update.getMessage().getChatId())
-                        .replyMarkup(InlineKeyboardMarkup.builder().keyboard(buttonsInit()).build())
-                        .build());
-
-            } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        if (update.hasCallbackQuery()) {
-            messageId = update.getCallbackQuery().getMessage().getMessageId();
+        if (update.hasCallbackQuery() && update.getCallbackQuery().getData() != null) {
             try {
                 callbackHandle(update);
             } catch (TelegramApiException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Ошибка обработки CallBack: ", e);
             }
 
-            for (String selectedWorker : selectedWorkers) {
-                try {
-                    execute(SendMessage.builder().chatId(update.getMessage().getChatId()).text(selectedWorker).build());
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
+        } else if (update.hasMessage() && update.getMessage().hasText()) {
+            try {
+                handler(update);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException("Ошибка обработки Message.text: ", e);
             }
         }
     }
 
-/*    public List<List<InlineKeyboardButton>> buttonsInit() {
-        List<InlineKeyboardButton> buttons = new ArrayList<>();
+    public void handler(Update update) throws TelegramApiException {
+        Message message = update.getMessage();
 
-        // Добавляем кнопки для работников из Employee
-        for (Employee value : Employee.values()) {
-            InlineKeyboardButton button = InlineKeyboardButton
-                    .builder()
-                    .callbackData(value.getName())
-                    .text(value.getName())
-                    .build();
-            buttons.add(button);
-            buttonStates.put(value.getName(), false);
+        if (message.getText().contains("/start")) {
+            execute(SendMessage.builder()
+                    .chatId(update.getMessage().getChatId()).text("Объект: ")
+                    .replyMarkup(InlineKeyboardMarkup.builder().keyboard(Keyboard.initBuildingKeyboard()).build())
+                    .build());
         }
-
-        // Разбиваем кнопки на ряды по две кнопки в каждом ряду
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        for (int i = 0; i < buttons.size(); i += 2) {
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            row.add(buttons.get(i));
-            if (i + 1 < buttons.size()) {
-                row.add(buttons.get(i + 1));
-            }
-            rows.add(row);
-        }
-
-        return rows;
-    }  Рабочий метод ButtonsInit*/
-/*    public List<List<InlineKeyboardButton>> buttonsInit() {
-        List<InlineKeyboardButton> buttons = new ArrayList<>();
-
-        // Добавляем кнопки для работников из Employee
-        for (Employee value : Employee.values()) {
-            boolean isSelected = buttonStates.getOrDefault(value.getName(), false);
-            String buttonText = isSelected ? " ✅" + value.getName() : "" + value.getName();
-
-            InlineKeyboardButton button = InlineKeyboardButton
-                    .builder()
-                    .callbackData(value.getName())
-                    .text(buttonText)
-                    .build();
-            buttons.add(button);
-        }
-
-        // Разбиваем кнопки на ряды по две кнопки в каждом ряду
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        for (int i = 0; i < buttons.size(); i += 2) {
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            row.add(buttons.get(i));
-            if (i + 1 < buttons.size()) {
-                row.add(buttons.get(i + 1));
-            }
-            rows.add(row);
-        }
-
-        return rows;
-    }  ЛУШИЙ BUTTONINIT с обновлением состояния кнопок сотрудников*/
-    public List<List<InlineKeyboardButton>> buttonsInit() {
-        List<InlineKeyboardButton> buttons = new ArrayList<>();
-
-        // Добавляем кнопки для работников из Employee
-        for (Employee value : Employee.values()) {
-            boolean isSelected = buttonStates.getOrDefault(value.getName(), false);
-            String buttonText = isSelected ? " ✅" + value.getName() : "" + value.getName();
-
-            InlineKeyboardButton button = InlineKeyboardButton
-                    .builder()
-                    .callbackData(value.getName())
-                    .text(buttonText)
-                    .build();
-            buttons.add(button);
-        }
-
-        // Разбиваем кнопки на ряды по две кнопки в каждом ряду
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        for (int i = 0; i < buttons.size(); i += 2) {
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            row.add(buttons.get(i));
-            if (i + 1 < buttons.size()) {
-                row.add(buttons.get(i + 1));
-            }
-            rows.add(row);
-        }
-
-        return rows;
     }
 
-    private void callbackHandle(Update update) throws TelegramApiException {
-        if (update.hasCallbackQuery()) {
-            String callbackData = update.getCallbackQuery().getData();
-            String buttonText = "";
-            boolean isSelected = false;
+    public void callbackHandle(Update update) throws TelegramApiException {
+        String data = update.getCallbackQuery().getData();
+        boolean clickBuildButton = false;
 
-            if (buttonStates.containsKey(callbackData)) {
-                isSelected = buttonStates.get(callbackData);
+        for (String value : Keyboard.buildingMap.values()) {
+            if (value.equals(data) && !data.equals("Готово")) {
+                clickBuildButton = true;
+                building = value;
+                break;
             }
+        }
 
-            // Инвертируем состояние и обновляем текст кнопки
-            isSelected = !isSelected;
-            buttonStates.put(callbackData, isSelected);
-
-            // Получаем текущий текст кнопки
-            buttonText = isSelected ? " ✅" + callbackData : "" + callbackData;
-
-            // Создаем объект EditMessageText только для обновления текста кнопки
-            EditMessageText editMessageText = EditMessageText
-                    .builder()
-                    .chatId(update.getCallbackQuery().getMessage().getChatId())
-                    .text("Бригада:") // Не изменяем это сообщение
+        if (clickBuildButton) {
+            execute(EditMessageText.builder().chatId(update.getCallbackQuery().getMessage().getChatId()) // меняю сообщения
                     .messageId(update.getCallbackQuery().getMessage().getMessageId())
-                    .replyMarkup(InlineKeyboardMarkup.builder()
-                            .keyboard(buttonsInit()) // Здесь вы можете использовать вашу текущую клавиатуру
-                            .build())
-                    .build();
-
-            // Отправляем запрос на обновление текста кнопки
-            execute(editMessageText);
-
-            // Теперь обновляем текст кнопки
-            EditMessageText editButton = EditMessageText
-                    .builder()
-                    .chatId(update.getCallbackQuery().getMessage().getChatId())
                     .text("Бригада: ")
+                    .build());
+
+            execute(EditMessageReplyMarkup.builder()   // меняю клавиатуру
                     .messageId(update.getCallbackQuery().getMessage().getMessageId())
-                    .replyMarkup(InlineKeyboardMarkup.builder()
-                            .keyboard(buttonsInit()) // Здесь вы можете использовать вашу текущую клавиатуру
-                            .build())
-                    .build();
-
-            // Отправляем запрос на обновление текста кнопки
-            execute(editButton);
+                    .chatId(update.getCallbackQuery().getMessage().getChatId())
+                    .replyMarkup(InlineKeyboardMarkup.builder().keyboard(Keyboard.initEmployeeKeyboard()).build())
+                    .build());
         }
-    }
+        if (data.equals("Готово")) {
+            execute(EditMessageText.builder().chatId(update.getCallbackQuery().getMessage().getChatId()) // меняю сообщения
+                    .messageId(update.getCallbackQuery().getMessage().getMessageId())
+                    .text("Выберите действие: ")
+                    .build());
 
-<<<<<<< HEAD
-//        if (update.hasCallbackQuery()) {
-//            for (Employee value : Employee.values()) {
-//                if (update.getCallbackQuery().getData().equals(value.getName())) {
-//                    // Добавляем или удаляем работника в зависимости от текущего состояния
-//                    boolean isWorkerSelected = selectedWorkers.contains(value.getName());
-//                    if (isWorkerSelected) {
-//                        selectedWorkers.remove(value.getName());
-//                    } else {
-//                        selectedWorkers.add(value.getName());
-//                    }
-//                    String buttonText = isWorkerSelected ? " ❌" + listToString(selectedWorkers) : " ✅" + value.getName();
-//
-//
-//                    // Проверяем, должен ли текст кнопки измениться
-//                    if (!buttonText.equals(update.getCallbackQuery().getMessage().getText())) {
-//                        // Создаем объект EditMessageText для обновления сообщения
-//                        EditMessageText editMessageText = EditMessageText
-//                                .builder()
-//                                .chatId(update.getCallbackQuery().getMessage().getChatId())
-//                                .text(buttonText)
-//                                .messageId(update.getCallbackQuery().getMessage().getMessageId())
-//                                .replyMarkup(InlineKeyboardMarkup.builder()
-//                                        .keyboard(buttonsInit()) // Здесь вы можете использовать вашу текущую клавиатуру
-//                                        .build())
-//                                .build();
-//
-//                        // Отправляем запрос на обновление сообщения
-//                        execute(editMessageText);
-//                    }
-//                }
-//            }
-//        }
-//    } ХЭНДЛ КОЛБЭК КОТОРЫЙ РАБОТАЕТ, НО ОБНОВЛЯЕТ ВЫБРАННЫХ СОТРУДНИКОВ, А НЕ ДОБАВЛЯЕТ ДРУГ К ДРУГУ
-//private void callbackHandle(Update update) throws TelegramApiException {
-//            if (update.hasCallbackQuery()) {
-//                String callbackData = update.getCallbackQuery().getData();
-//                String buttonText = "";
-//                boolean isSelected = false;
-//
-//                if (buttonStates.containsKey(callbackData)) {
-//                    isSelected = buttonStates.get(callbackData);
-//                }
-//
-//                // Инвертируем состояние и обновляем текст кнопки
-//                isSelected = !isSelected;
-//                buttonStates.put(callbackData, isSelected);
-//
-//                // Обновляем текст кнопки в сообщении
-//                buttonText = isSelected ? " ✅" + callbackData : "" + callbackData;
-//
-//                // Создаем объект EditMessageText для обновления сообщения
-//                EditMessageText editMessageText = EditMessageText
-//                        .builder()
-//                        .chatId(update.getCallbackQuery().getMessage().getChatId())
-//                        .text(buttonText)
-//                        .messageId(update.getCallbackQuery().getMessage().getMessageId())
-//                        .replyMarkup(InlineKeyboardMarkup.builder()
-//                                .keyboard(buttonsInit()) // Здесь вы можете использовать вашу текущую клавиатуру
-//                                .build())
-//                        .build();
-//
-//                // Отправляем запрос на обновление сообщения
-//                execute(editMessageText);
-//            }
-//        } ЛУЧШИЙ НА ДАННЫЙ МОМЕНТ ХЭНДЛ КОЛБЭЕ, ОБНОВЛЯЕТ КНОПКИ, НО И ТЕКС ОБНОВЛЯЕТ (А ЭТОГО НЕ НАДО)
-}
-=======
-    public String listToString(List<String> list) {
-        StringBuilder result = new StringBuilder();
-        int index = 0;
-
-        for (String item : list) {
-            boolean found = false;
-            for (Employee employee : Employee.values()) {
-                if (employee.getName().equals(item)) {
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                result.append("\t\t\t\t").append(++index).append(") ").append(item).append("\n");
-            }
+            execute(EditMessageReplyMarkup.builder()
+                    .messageId(update.getCallbackQuery().getMessage().getMessageId()) // меняю клавиатуру
+                    .chatId(update.getCallbackQuery().getMessage().getChatId())
+                    .replyMarkup(InlineKeyboardMarkup.builder().keyboard(Keyboard.readyBackKeyboard()).build())
+                    .build());
         }
-        return result.toString();
+        if (data.equals("Изменить объект")) {
+            execute(EditMessageText.builder().chatId(update.getCallbackQuery().getMessage().getChatId()) // меняю сообщения
+                    .messageId(update.getCallbackQuery().getMessage().getMessageId())
+                    .text("Объект: ")
+                    .build());
+
+            execute(EditMessageReplyMarkup.builder()   // меняю клавиатуру
+                    .messageId(update.getCallbackQuery().getMessage().getMessageId())
+                    .chatId(update.getCallbackQuery().getMessage().getChatId())
+                    .replyMarkup(InlineKeyboardMarkup.builder().keyboard(Keyboard.initBuildingKeyboard()).build())
+                    .build());
+        } 
     }
 }
->>>>>>> c5236bc2edfdc15bcbd54a853e9414b6300bf4f3
+
---
+++ src/main/java/org/example/Keyboard.java
@@ -0,0 +1,97 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.*;
+
+public class Keyboard {
+    protected static Map<Integer, String> employee = new HashMap<>();
+    protected static Map<Integer, String> buildingMap = new HashMap<>();
+    protected static Map<Integer, String> readyBackMap = new HashMap<>();
+
+    public static List<List<InlineKeyboardButton>> initBuildingKeyboard() {
+        List<List<InlineKeyboardButton>> buildingKeyboard = new ArrayList<>();
+
+        buildingMap.put(1, "ЖК Суворов");
+        buildingMap.put(2, "ТЦ Доваторцев 65");
+        buildingMap.put(3, "Автомойка Доваторцев 65");
+        buildingMap.put(4, "Герцена 147");
+        buildingMap.put(5, "Южный Обход 1/2");
+        buildingMap.put(6, "Роддом");
+
+        for (String value : buildingMap.values()) {
+            buildingKeyboard.add(Collections.singletonList(InlineKeyboardButton.builder()
+                    .callbackData(value)
+                    .text(value)
+                    .build()));
+        }
+        return buildingKeyboard;
+    }
+
+    public static List<List<InlineKeyboardButton>> initEmployeeKeyboard() {
+        List<List<InlineKeyboardButton>> employeeKeyboard = new ArrayList<>();
+
+        employee.put(1, "Акинин О.");
+        employee.put(2, "Гостищев М.");
+        employee.put(3, "Гусаров А.");
+        employee.put(4, "Ермаков Д.");
+        employee.put(5, "Новоселов А.");
+        employee.put(6, "Скиба А.");
+        employee.put(7, "Толстенев А.");
+        employee.put(8, "Шушпанников А.");
+        employee.put(9, "Жидков М.");
+        employee.put(10, "Ивакин Ю.");
+        employee.put(11, "Коваленко В.");
+        employee.put(12, "Мартынов К.");
+        employee.put(13, "Моисеенко А.");
+        employee.put(14, "Панасейко Ю.");
+        employee.put(15, "Подольский В.");
+        employee.put(16, "Труфанов В.");
+        employee.put(17, "Готово");
+
+        int columnCount = 2; // Количество столбцов
+        int rowCount = (int) Math.ceil(employee.size() / (double) columnCount);
+
+        for (int i = 0; i < rowCount; i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            for (int j = 0; j < columnCount; j++) {
+                int index = i * columnCount + j + 1;
+
+                if (index > employee.size()) {
+                    break;
+                }
+
+                String value = employee.get(index);
+                row.add(InlineKeyboardButton.builder()
+                        .callbackData(value)
+                        .text(value)
+                        .build());
+            }
+            employeeKeyboard.add(row);
+        }
+
+        return employeeKeyboard;
+    }
+
+    public static List<List<InlineKeyboardButton>> readyBackKeyboard() {
+        List<List<InlineKeyboardButton>> readyBackKeyboard = new ArrayList<>();
+
+        readyBackMap.put(1, "Изменить объект");
+        readyBackMap.put(2, "Изменить состав бригады");
+        readyBackMap.put(3, "Отправить");
+
+        for (String value : readyBackMap.values()) {
+            readyBackKeyboard.add(Collections.singletonList(InlineKeyboardButton.builder()
+                    .callbackData(value)
+                    .text(value)
+                    .build()));
+        }
+        return readyBackKeyboard;
+    }
+}
+
+
+
+
+
